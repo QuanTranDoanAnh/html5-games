22dd22c84455e690e800566ba3510dc3c1fb99f1
QuanTranDoanAnh <td.anhquan@gmail.com>
2025-11-09 08:33:27 +0700

    Chapter 1: Getting Started with DOM-based Game Development - Listening inputs

--- pingpong/js/html5games.pingpong.js
+++ pingpong/js/html5games.pingpong.js
@@ -1,38 +1,52 @@
-// code inside $(function(){} will run after the DOM is loaded and
-// ready
-var KEY = {
-  UP: 38,
-  DOWN: 40,
+const KEY = {
   W: 87,
   S: 83,
+  UP: 38,
+  DOWN: 40,
 };
-$(function() {
-    // listen to keydown event
-    $(document).keydown(function(e) {
-        switch (e.which) {
-            case KEY.UP: // up arrow
-                // get the current paddle B's top value in Int type
-                var top = parseInt($("#paddleB").css("top"));
-                // move the paddle B up 5 pixels
-                $("#paddleB").css("top", top - 5);
-                break;
-            case KEY.DOWN: // down arrow
-                var top = parseInt($("#paddleB").css("top"));
-                // move the paddle B down 5 pixels
-                $("#paddleB").css("top", top + 5);
-                break;
-            case KEY.W: // up arrow
-                // get the current paddle B's top value in Int type
-                var top = parseInt($("#paddleA").css("top"));
-                // move the paddle B up 5 pixels
-                $("#paddleA").css("top", top - 5);
-                break;
-            case KEY.S: // down arrow
-                var top = parseInt($("#paddleA").css("top"));
-                // move the paddle B down 5 pixels
-                $("#paddleA").css("top", top + 5);
-                break;
-                
-        }
-    })
-})
+var pingpong = {};
+pingpong.pressedKeys = [];
+
+$(function () {
+  // set interval to call game loop every 30 milliseconds
+  pingpong.timer = setInterval(gameLoop, 30);
+
+  // mark down what key is down and up into an array called "pressedKeys"
+  $(document).keydown(function (e) {
+    pingpong.pressedKeys[e.which] = true;
+  });
+  $(document).keyup(function (e) {
+    pingpong.pressedKeys[e.which] = false;
+  });
+});
+
+function gameLoop() {
+  movePaddles();
+}
+
+function movePaddles() {
+  // use our custom timer to continuously check if a key is pressed.
+  if (pingpong.pressedKeys[KEY.UP]) {
+    // move the paddle B up 5 pixels
+    var top = parseInt($("#paddleB").css("top"));
+    $("#paddleB").css("top", top - 5);
+  }
+  if (pingpong.pressedKeys[KEY.DOWN]) {
+    // arrow-down
+    // move the paddle B down 5 pixels
+    var top = parseInt($("#paddleB").css("top"));
+    $("#paddleB").css("top", top + 5);
+  }
+  if (pingpong.pressedKeys[KEY.W]) {
+    // w
+    // move the paddle A up 5 pixels
+    var top = parseInt($("#paddleA").css("top"));
+    $("#paddleA").css("top", top - 5);
+  }
+  if (pingpong.pressedKeys[KEY.S]) {
+    // s
+    // move the paddle A down 5 pixels
+    var top = parseInt($("#paddleA").css("top"));
+    $("#paddleA").css("top", top + 5);
+  }
+}

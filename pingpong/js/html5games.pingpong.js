4c645984d73de75b06949d0115ccaac20c51153c
QuanTranDoanAnh <td.anhquan@gmail.com>
2025-11-09 08:24:47 +0700

    Chapter 1: Getting Started with DOM-based Game Development - Key Press

--- pingpong/js/html5games.pingpong.js
+++ pingpong/js/html5games.pingpong.js
@@ -1,6 +1,38 @@
 // code inside $(function(){} will run after the DOM is loaded and
 // ready
+var KEY = {
+  UP: 38,
+  DOWN: 40,
+  W: 87,
+  S: 83,
+};
 $(function() {
-    $("#paddleB").css("top", "20px");
-    $("#paddleA").css("top", "60px");
-});
+    // listen to keydown event
+    $(document).keydown(function(e) {
+        switch (e.which) {
+            case KEY.UP: // up arrow
+                // get the current paddle B's top value in Int type
+                var top = parseInt($("#paddleB").css("top"));
+                // move the paddle B up 5 pixels
+                $("#paddleB").css("top", top - 5);
+                break;
+            case KEY.DOWN: // down arrow
+                var top = parseInt($("#paddleB").css("top"));
+                // move the paddle B down 5 pixels
+                $("#paddleB").css("top", top + 5);
+                break;
+            case KEY.W: // up arrow
+                // get the current paddle B's top value in Int type
+                var top = parseInt($("#paddleA").css("top"));
+                // move the paddle B up 5 pixels
+                $("#paddleA").css("top", top - 5);
+                break;
+            case KEY.S: // down arrow
+                var top = parseInt($("#paddleA").css("top"));
+                // move the paddle B down 5 pixels
+                $("#paddleA").css("top", top + 5);
+                break;
+                
+        }
+    })
+})
